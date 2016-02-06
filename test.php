babdd62330295cbe6bdc6f24a3acdedd98929636
kadrim1 <kadri.mihhailova@khk.ee>
2016-02-06 21:27:57 +0200

    Lecture 4: AJAX with PHP Using jQuery - Registration Form Project

--- test.php
+++ test.php
@@ -1,7 +1,27 @@
 <?php
 
+$con = mysqli_connect("localhost", "root", "", "test");
 
-echo $_POST['name'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
 
+$sel = "select * from users where email='$email'";
+
+$run = mysqli_query($con, $sel);
+
+$check_email = mysqli_num_rows($run);
+if ($check_email == 1) {
+    echo "<h2> This email is already redistred, please try another!</h2>";
+    exit();
+}//end if
+else {
+    $insert = "insert into users (name, pass, email) values ('$name','$pass','$email')";
+    $run_insert = mysqli_query($con, $insert);
+
+    if ($run_insert) {
+        echo "<h2>Registration Successful, Thanks!</h2>";
+    }; //end else if
+}; //end else
 
 ?>
